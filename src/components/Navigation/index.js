2101c365cd474c9d478f7a2267e1e4051b5e36fb
Sandra <sandra.iriandini@gmail.com>
2020-04-22 16:16:50 +0200

    Add recipes in the navigation bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -22,7 +22,7 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="About" />
-          <NavbarItem path="/other" linkText="Other" />
+          <NavbarItem path="/recipes" linkText="Recipes" />
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
