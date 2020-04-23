e41f2e95979d7931da0a5b0595296db85d96a8ed
Sandra <sandra.iriandini@gmail.com>
2020-04-23 14:24:26 +0200

    Create MyHomepage pages

---
+++ src/pages/MyHomepage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyHomepageSettingForm from "../../components/MyHomepageSettingForm";
+
+export default function MyHomepage() {
+  return (
+    <div>
+      <h2>Personal Data</h2>
+      <MyHomepageSettingForm />
+    </div>
+  );
+}
